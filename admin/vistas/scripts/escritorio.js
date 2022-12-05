325c349547c6941fc1e03009cacfcf32031f0aa2
Junior Cercado Vasquez <juniorcercado@upeu.edu.pe>
2022-12-04 22:01:55 -0500

    escritorio

--- admin/vistas/scripts/escritorio.js
+++ admin/vistas/scripts/escritorio.js
@@ -37,60 +37,36 @@ function tbla_principal() {
     e = JSON.parse(e);  console.log(e);   
     if (e.status == true) {
 
-      $('#div-todo-sobre-mi').html(`
-        <strong class="text-success"><i class="fas fa-book mr-1 text-success"></i> CIP</strong>
-        <p class="text-muted">${e.data.colegiado.codigo_cip}</p>
-
-        <hr class="my-1">
-
-        <strong class="text-success"><i class="fa-regular fa-envelope mr-1 text-success"></i> Correo</strong>
-        <p class="text-muted">${e.data.colegiado.email}</p>
-
-        <hr class="my-1">
-
-        <strong class="text-success"><i class="fa-regular fa-calendar-days mr-1 text-success"></i> Fecha Incorporacion</strong>
-        <p class="text-muted">${format_d_m_a(e.data.colegiado.fecha_incorporacion)}</p>
-
-        <hr class="my-1">
-
-        <strong class="text-success"><i class="fa-regular fa-id-badge mr-1 text-success"></i> Capitulo</strong>
-        <p class="text-muted">${e.data.colegiado.capitulo}</p>
-      `);
+      // $('#div-todo-sobre-mi').html(`  `);
       
       if (e.data.esposo == null ) {
         $("#div-esposo").append('─ Asigna a tu esposo(a), si no lo tiene omita este mensaje.');
       }else{
         $('#actualizado-esposo').html(`Actualizado el: ${e.data.esposo.updated_at}`);
+        
         $("#div-esposo").append(`
-          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+          <div class="col-12 col-sm-3 col-md-4 d-flex align-items-stretch flex-column">
             <div class="card bg-light d-flex flex-fill">
               <div class="card-header text-muted border-bottom-0">
-                Digital Strategist
+                Datos
               </div>
               <div class="card-body pt-0">
                 <div class="row">
                   <div class="col-7">
-                    <h2 class="lead"><b>Nicole Pearson</b></h2>
-                    <p class="text-muted text-sm"><b>About: </b> Web Designer / UX / Graphic Artist / Coffee Lover </p>
+                    <h2 class="lead"><b>${e.data.esposo.nombres}</b></h2>
+                    <p class="text-muted text-sm"><b>DNI: </b> ${e.data.esposo.dni} </p>
                     <ul class="ml-4 mb-0 fa-ul text-muted">
-                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
-                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
+                      <li class="small"><span class="fa-li"><i class="fa-solid fa-calendar-day"></i></span> Nac.: ${e.data.esposo.fecha_nacimiento}</li>
+                      <li class="small"><span class="fa-li"><i class="fa-solid fa-venus-mars"></i></span> Sexo: ${e.data.esposo.sexo}</li>
                     </ul>
                   </div>
                   <div class="col-5 text-center">
-                    <img src="../../dist/img/user1-128x128.jpg" alt="user-avatar" class="img-circle img-fluid">
+                    ${e.data.esposo.sexo == 'M'? '<i class="fa-solid fa-person fa-4x"></i>' : '<i class="fa-solid fa-person-dress fa-4x"></i>' }
                   </div>
                 </div>
               </div>
               <div class="card-footer">
-                <div class="text-right">
-                  <a href="#" class="btn btn-sm bg-teal">
-                    <i class="fas fa-comments"></i>
-                  </a>
-                  <a href="#" class="btn btn-sm btn-primary">
-                    <i class="fas fa-user"></i> View Profile
-                  </a>
-                </div>
+                <div class="text-right"> <a href="datos_hijos.php" target="_blank" class="btn btn-sm bg-teal"><i class="fas fa-comments"></i> </a> </div>
               </div>
             </div>
           </div>
@@ -100,41 +76,40 @@ function tbla_principal() {
       if (e.data.hijos == null) {
         $("#div-hijos").append('─ Asigna a tus hijos, si no lo tiene omita este mensaje.');
       } else {
-        $('#actualizado-hijos').html(`Actualizado el: ${e.data.hijos.updated_at}`);
-        $("#div-hijos").append(`
-          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
-            <div class="card bg-light d-flex flex-fill">
-              <div class="card-header text-muted border-bottom-0">
-                Digital Strategist
-              </div>
-              <div class="card-body pt-0">
-                <div class="row">
-                  <div class="col-7">
-                    <h2 class="lead"><b>Nicole Pearson</b></h2>
-                    <p class="text-muted text-sm"><b>About: </b> Web Designer / UX / Graphic Artist / Coffee Lover </p>
-                    <ul class="ml-4 mb-0 fa-ul text-muted">
-                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
-                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
-                    </ul>
-                  </div>
-                  <div class="col-5 text-center">
-                    <img src="../../dist/img/user1-128x128.jpg" alt="user-avatar" class="img-circle img-fluid">
+        
+        var hijos_html = "";
+        e.data.hijos.forEach((val, key) => {
+          hijos_html = hijos_html.concat(`
+            <div class="col-12 col-sm-3 col-md-4 d-flex align-items-stretch flex-column">
+              <div class="card bg-light d-flex flex-fill">
+                <div class="card-header text-muted border-bottom-0">
+                  Datos
+                </div>
+                <div class="card-body pt-0">
+                  <div class="row">
+                    <div class="col-7">
+                      <h2 class="lead"><b>${val.nombres} ${val.apellidos}</b></h2>
+                      <p class="text-muted text-sm"><b>DNI: </b> ${val.dni} </p>
+                      <ul class="ml-4 mb-0 fa-ul text-muted">
+                        <li class="small"><span class="fa-li"><i class="fa-solid fa-calendar-day"></i></span> Nac.: ${val.fecha_nacimiento}</li>
+                        <li class="small"><span class="fa-li"><i class="fa-solid fa-venus-mars"></i></span> Sexo: ${val.sexo}</li>
+                      </ul>
+                    </div>
+                    <div class="col-5 text-center">
+                      ${val.sexo == 'M'? '<i class="fa-solid fa-person fa-4x"></i>' : '<i class="fa-solid fa-person-dress fa-4x"></i>' }
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="card-footer">
-                <div class="text-right">
-                  <a href="#" class="btn btn-sm bg-teal">
-                    <i class="fas fa-comments"></i>
-                  </a>
-                  <a href="#" class="btn btn-sm btn-primary">
-                    <i class="fas fa-user"></i> View Profile
-                  </a>
+                <div class="card-footer">
+                  <div class="text-right"> <a href="datos_hijos.php" target="_blank" class="btn btn-sm bg-teal"><i class="fas fa-comments"></i> </a> </div>
                 </div>
               </div>
             </div>
-          </div>
-        `);
+          `);
+          
+        });
+
+        $("#div-hijos").html(hijos_html);
       }
 
       if (e.data.experiencia.length === 0) {
